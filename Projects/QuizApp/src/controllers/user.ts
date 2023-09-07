dff734575b3317ae394f6295246ae290afc7062b
Suyash <suyashgehlot2@gmail.com>
2023-09-07 16:19:23 +0530

    Added different routes for user model

--- Projects/QuizApp/src/controllers/user.ts
+++ Projects/QuizApp/src/controllers/user.ts
@@ -27,4 +27,49 @@ const registerUser = async (req : Request,res : Response,next : NextFunction) =>
     }
 }
 
-export {registerUser};
+const getUser = async (req : Request, res : Response) => {
+    let resp : returnResponse;
+    try{
+        const userId = req.params.userId;
+        const user = await User.findById(userId,{name : 1,email : 1});
+        if(!user){
+            resp = {status : "Error", message : "Something went wrong",data : {}};
+            res.send(resp);
+        }
+        else{
+            resp =  {status : "Success", message : "User Found",data : user};
+            res.send(resp);
+        }
+    } 
+    catch(error) {
+        console.log(error);
+        resp = {status : "Error", message : "Something went wrong",data : {}};
+        res.status(500).send(resp);
+    }
+}
+
+const updateUser = async (req : Request, res : Response) => {
+    let resp : returnResponse;
+    try {
+        const userId = req.body._id;
+        const user = await User.findById(userId);
+
+        if(!user){
+            resp = {status : "Error", message : "Something went wrong",data : {}};
+            res.send(resp);
+        }
+        else{
+            user.name = req.body.name;
+            await user.save();
+            resp =  {status : "Success", message : "User Updated",data : {}};
+            res.send(resp);
+        }
+    } 
+    catch(error) {
+        console.log(error);
+        resp = {status : "Error", message : "Something went wrong",data : {}};
+        res.status(500).send(resp);
+    }
+}
+
+export {registerUser, getUser , updateUser};
