ecf45d14414764aa09b5775d81fc0c5a5d0d677f
Suyash <suyashgehlot2@gmail.com>
2023-09-17 02:09:37 +0530

    Learnt how routing works in react and how to use react-router-dom

---
+++ React/my-project/src/App.js
@@ -0,0 +1,19 @@
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ React/my-project/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      I am on about page
+    </div>
+  )
+}
+
+export default About
