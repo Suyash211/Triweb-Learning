9f08b06b56014ffcf43810144d6aa27ed51d011c
Suyash <suyashgehlot2@gmail.com>
2023-09-18 20:03:53 +0530

    Learnt how to create menu using link and how to style react components

--- React/my-project/src/App.js
+++ React/my-project/src/App.js
@@ -1,11 +1,13 @@
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import MainMenu from "./components/layouts/MainMenu";
 import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+    <MainMenu />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
