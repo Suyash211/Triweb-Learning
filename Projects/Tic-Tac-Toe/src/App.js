1b38b712721985210df667516ed71b62562328aa
Suyash <suyashgehlot2@gmail.com>
2023-09-27 16:05:07 +0530

    Added onClick functionality to the tic-tac-toe board

--- Projects/Tic-Tac-Toe/src/App.js
+++ Projects/Tic-Tac-Toe/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
-function Square(props) {
+function Square() {
+  const [value,setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
   return(
-    <button className="square">{props.value}</button>
+    <button className="square" onClick={handleClick}>{value}</button>
   );
 }
 
@@ -10,21 +17,21 @@ export default function Board() {
   return(
     <div>
       <div className="board-row">
-        <Square value='1'/>
-        <Square value='2'/>
-        <Square value='3'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       
       <div className="board-row">
-        <Square value='4'/>
-        <Square value='5'/>
-        <Square value='6'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <Square value='7'/>
-        <Square value='8'/>
-        <Square value='9'/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
   );
