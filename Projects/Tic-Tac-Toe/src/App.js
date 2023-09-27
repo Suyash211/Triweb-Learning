632ec0d88e9da9c96bb6b31dd7995776a0250913
Suyash <suyashgehlot2@gmail.com>
2023-09-27 16:37:35 +0530

    Improved the code and made X and O alternating

--- Projects/Tic-Tac-Toe/src/App.js
+++ Projects/Tic-Tac-Toe/src/App.js
@@ -1,37 +1,46 @@
 import React from "react";
 import { useState } from "react";
 
-function Square() {
-  const [value,setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-  }
+function Square(props) {
 
   return(
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={props.onSquareClick}>{props.value}</button>
   );
+
 }
 
 export default function Board() {
+  const [xIsNext,setXIsNext] = useState(true);
+  const [squares,setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquare = squares.slice();
+    if(xIsNext)
+      nextSquare[i] = 'X';
+    else
+      nextSquare[i] = 'O';
+    setXIsNext(!xIsNext);
+    setSquares(nextSquare);
+  }
+
   return(
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
