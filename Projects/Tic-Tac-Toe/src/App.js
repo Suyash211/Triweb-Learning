a7bc43a39c71775f975c8b0b37e337a2f72c0936
Suyash <suyashgehlot2@gmail.com>
2023-09-28 18:32:05 +0530

    Implemented Time-Travel functionality to the game

--- Projects/Tic-Tac-Toe/src/App.js
+++ Projects/Tic-Tac-Toe/src/App.js
@@ -29,50 +29,94 @@ function calculateWinner(squares){
   return null;
 }
 
-export default function Board() {
+export default function Game(){
   const [xIsNext,setXIsNext] = useState(true);
-  const [squares,setSquares] = useState(Array(9).fill(null));
+  const [history,setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove,setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0)
+  }
+
+  const moves = history.map((squares,move) => {
+    let description;
+    if(move > 0){
+      description = "Go to move #" + move;
+    }
+    else{
+      description = "Go to game start";
+    }
+    return(
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+function Board(props) {
   
-  const winner = calculateWinner(squares);
+  const winner = calculateWinner(props.squares);
   let status;
   if(winner){
     status = "Winner : " + winner;
   }
   else{
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next player: " + (props.xIsNext ? "X" : "O");
   }
 
   function handleClick(i){
-    if(calculateWinner(squares) || squares[i])
+    if(calculateWinner(props.squares) || props.squares[i])
       return;
-    const nextSquare = squares.slice();
-    if(xIsNext)
+    const nextSquare = props.squares.slice();
+    if(props.xIsNext){
       nextSquare[i] = 'X';
-    else
+    }
+    else{
       nextSquare[i] = 'O';
-    setXIsNext(!xIsNext);
-    setSquares(nextSquare);
+    }
+    props.onPlay(nextSquare);
   }
 
   return(
     <div>
       <div className="status"> {status} </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={props.squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={props.squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={props.squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={props.squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={props.squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={props.squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={props.squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={props.squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={props.squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
