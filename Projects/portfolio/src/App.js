a98c744601b600b709d2d93bafb0f29384d4cc7f
Suyash <suyashgehlot2@gmail.com>
2023-10-09 19:45:33 +0530

    Created navbar for protfolio

--- Projects/portfolio/src/App.js
+++ Projects/portfolio/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
 import Home from './containers/home';
 import About from './containers/about';
 import Contact from './containers/contact';
@@ -10,6 +11,9 @@ import Skills from './containers/skills';
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
+
       <Routes>
         <Route index path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
@@ -18,6 +22,7 @@ function App() {
         <Route path='/resume' element={<Resume/>}/>
         <Route path='/skills' element={<Skills/>}/>
       </Routes>
+
     </div>
   );
 }
