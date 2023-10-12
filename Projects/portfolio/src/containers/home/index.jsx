605012b96e3deec040ed8349eecd4191125582d0
Suyash <suyashgehlot2@gmail.com>
2023-10-12 18:44:34 +0530

    Added a hire me button in home page which redirect to contact page

--- Projects/portfolio/src/containers/home/index.jsx
+++ Projects/portfolio/src/containers/home/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./styles.scss"
+import { Animate } from "react-simple-animate";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+
+    const navigate = useNavigate();
+    
+    const handleNavigateContactMePage = () => {
+        navigate('/contact')
+    }
     return(
         <section id="home" className="home">
             <div className="home__text-wrapper">
@@ -10,6 +18,23 @@ const Home = () => {
                 Front end developer
                 </h1>
             </div>
+            <Animate
+                play
+                duration={1.5}
+                delay={1}
+                start={{
+                    transform: "translateY(550px)"
+                }}
+                end={{
+                    transform: "translatex(0px)"
+                }}
+            >
+                <div className="home__contact-me">
+                    <button onClick={handleNavigateContactMePage}>
+                        Hire Me
+                    </button>
+                </div>
+            </Animate>
         </section>
     )
 }
