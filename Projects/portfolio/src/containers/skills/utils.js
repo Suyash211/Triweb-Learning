8ff659ad104931274c3359019df870a5f6b92317
Suyash <suyashgehlot2@gmail.com>
2023-10-16 02:28:38 +0530

    Added content to the skills page

---
+++ Projects/portfolio/src/containers/skills/utils.js
@@ -0,0 +1,45 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "NODEJS",
+          percentage: "70",
+        },
+        {
+          skillName: "C++",
+          percentage: "65",
+        }
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "40",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "60",
+        }
+      ],
+    },
+  ];
