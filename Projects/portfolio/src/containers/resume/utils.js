5bdba0797bc7d0440d98aa8a1c4c1afd2b8a00c5
Suyash <suyashgehlot2@gmail.com>
2023-10-16 19:29:16 +0530

    Added content to the Resume page

---
+++ Projects/portfolio/src/containers/resume/utils.js
@@ -0,0 +1,21 @@
+export const data = {
+    experience: [
+      {
+        title: "TriWeb API",
+        description:
+          " Web Development Intern",
+      },
+    ],
+    education: [
+      {
+        title: "Brightlands School",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "Graphic Era Deemed To Be University",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
